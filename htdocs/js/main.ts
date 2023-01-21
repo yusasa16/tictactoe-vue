907cd257a3d0b01546c9d8d04a999bda999a25de
yusasa16 <yusasa16@gmail.com>
2023-01-21 21:57:27 +0900

    vue 追加

--- htdocs/js/main.ts
+++ htdocs/js/main.ts
@@ -1,5 +1,8 @@
 import 'normalize.css'
+import { createApp } from 'vue'
+// @ts-ignore
+import App from './vue/App.vue'
 
 window.addEventListener('load', function() {
-
+	createApp(App).mount('#app')
 });
---
+++ htdocs/js/vue/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const test = ref('test');
+</script>
+
+<template>
+	<div>
+		{{ test }}
+	</div>
+</template>
